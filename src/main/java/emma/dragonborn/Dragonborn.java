0fc1bd4ca2968957659cfdcb9c27589eab81cd0b
Emmakasaki <emilymakistan@gmail.com>
2025-08-21 01:13:52 -0700

    Initial commit

---
+++ src/main/java/emma/dragonborn/Dragonborn.java
@@ -0,0 +1,18 @@
+package emma.dragonborn;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Dragonborn implements ModInitializer {
+	public static final String MOD_ID = "dragonborn";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
---
+++ src/main/java/emma/dragonborn/DragonbornModClient.java
@@ -0,0 +1,11 @@
+package emma.dragonborn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DragonbornModClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
